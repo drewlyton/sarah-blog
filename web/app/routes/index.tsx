461670c7b9397858d28e385f4602f36611554e20
Drew Lytle <aml3ed@virginia.edu>
2022-08-09 16:46:35 -0400

    feat: Add tailwind

--- web/app/routes/index.tsx
+++ web/app/routes/index.tsx
@@ -1,32 +1,30 @@
+import { Link, useLoaderData } from "@remix-run/react";
+import { getClient } from "~/lib/sanity/client";
+
+type LoaderData = {
+  posts: Array<{ _id: string; title: string; slug: { current: string } }>;
+};
+
+export async function loader(): Promise<LoaderData> {
+  const posts = await getClient().fetch(
+    `*[_type == "post"]{ _id, title, slug }`
+  );
+
+  return { posts };
+}
+
 export default function Index() {
+  let { posts } = useLoaderData<LoaderData>();
+
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+    <div style={{ textAlign: "center", padding: 20 }}>
+      {posts?.length >= 1
+        ? posts.map((post) => (
+            <div style={{ padding: 10 }} key={post._id}>
+              <Link to={post.slug.current}>{post.title}</Link>
+            </div>
+          ))
+        : null}
     </div>
   );
 }
